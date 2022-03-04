62b214bbd513da5bea8df5dfeb122a6cd3e1f90a
Nelson Jawuer Morales Santa <guitarro92@hotmail.com>
2022-03-04 14:30:56 -0500

    Primeras pruebas.

---
+++ main.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Plugin Name: Pictures Jw
+Plugin URI: https://github.com/jawuermkr/wp-plug-picJw
+Description: This is a simple helper for add pictures. Made for request job.
+Version: 1.0.0
+Author URI: http://jawuer.com/
+*/
+
+add_shortcode('pic', function () {
+  $output = '
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="..." alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+  ';
+  return $output;
+} );
+
+/*
+Carrucel desde bootstrap, implementación por shortcode [pic]
+*/
+?>
